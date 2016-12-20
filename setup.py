18b187b75c67f1bd040b2561e06e94edcb2e5c8e
Yask Srivastava <yask123@gmail.com>
2016-12-20 19:09:56 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="codepair",
+	version="1.1",
+	description="Instantly practice questions on HackerRank codepair",
+	url="http://www.github.com/yask123",
+	author="Yask Srivastava",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["codepair"],
+	scripts=["bin/codepair"],
+	zip_safe=False)
